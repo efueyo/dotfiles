2c30a2a5eae7a6edc02f75e1388c635d95e23420
Enrique Fueyo <efueyo@lang.ai>
2022-04-02 12:59:09 -0700

    moved lspconfig from vim to lua

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -1,15 +1,12 @@
-if !exists('g:lspconfig') | finish | endif
-
-lua << EOF
-
 local my_lsp = require("user.lsp")
+
 my_lsp.setup()
+
 local servers = {'pyright', 'gopls', 'tsserver'}
+
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = my_lsp.on_attach,
     capabilities = my_lsp.capabilities,
   }
 end
-
-EOF
