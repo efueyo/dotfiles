35810b0774e3f2854ca3137341d7ba619d8b186f
Enrique Fueyo <efueyo@lang.ai>
2022-04-06 21:15:54 -0700

    feat(lsp): added support por jsonls

--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -20,6 +20,10 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", gopls_opts, opts)
 	end
 
+	if server.name == "jsonls" then
+		local jsonls_opts = require("user.lsp.settings.jsonls")
+		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	end
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
