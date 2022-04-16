f8db26e7d323ecc809b7c459813dd77f63878e35
Enrique Fueyo <efueyo@lang.ai>
2022-04-16 15:24:37 -0700

    chore(treesitter): remove unused code

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -17,5 +17,3 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.used_by = { "javascript", "typescript.tsx" }
