34971420fa56a0025e4a41e55b86fa78977615f7
Enrique Fueyo <efueyo@lang.ai>
2022-05-04 16:25:45 -0700

    feat(null-ls): silent autocomands to avoid annoying errors in some projects

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -32,7 +32,7 @@ null_ls.setup({
       vim.cmd([[
         augroup LspFormatting
             autocmd! * <buffer>
-            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            autocmd BufWritePre <buffer> silent! lua vim.lsp.buf.formatting_sync()
         augroup END
       ]])
     end
