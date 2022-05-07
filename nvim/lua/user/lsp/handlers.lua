4fd56165d44c08b932a4d212a1955dd9c096efce
Enrique Fueyo <efueyo@lang.ai>
2022-05-07 14:52:17 -0700

    chore(null-ls): rever silent! some autocomands

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -49,8 +49,8 @@ local function lsp_highlight_document(client)
       [[
       augroup lsp_document_highlight
         autocmd! * <buffer>
-        autocmd CursorHold <buffer> silent! lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> silent! lua vim.lsp.buf.clear_references()
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
       false
