92d91c424d4a3172f95e7317e99443cb328e4942
Enrique Fueyo <efueyo@lang.ai>
2022-07-01 16:03:39 -0700

    feat(go): add <leader>ts to go to test_suite file

--- nvim/after/ftplugin/go/alternate.lua
+++ nvim/after/ftplugin/go/alternate.lua
@@ -2,4 +2,11 @@ local go_to_alternate = require"user.alternate"
 
 vim.keymap.set("n", "<leader>ga", function () go_to_alternate(".go", "_test.go") end)
 vim.keymap.set("n", "<leader>gav", function () go_to_alternate(".go", "_test.go", "vsplit") end)
+-- go to test suite file
+local go_to_test_suite = function ()
+  local test_suite_path = vim.fn.expand("%:h") .. "/*suite_test.go"
+  vim.cmd("e ".. test_suite_path)
+
+end
+vim.keymap.set("n", "<leader>ts", function () go_to_test_suite() end)
 
