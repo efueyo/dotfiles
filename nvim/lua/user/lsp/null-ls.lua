1957aa62fcb3ef7738a027135b618b403c3df128
Enrique Fueyo <efueyo@lang.ai>
2022-07-07 16:35:35 -0700

    fix(null-ls): fix prettier and null-ls format on save

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -5,6 +5,12 @@ end
 
 local formatting = null_ls.builtins.formatting
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    bufnr = bufnr,
+  })
+end
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {clear = true})
 null_ls.setup({
 	debug = false,
 	sources = {
@@ -27,14 +33,17 @@ null_ls.setup({
       }
     }),
 	},
-  on_attach = function(client)
-    if client.server_capabilities.document_formatting then
-      vim.cmd([[
-        augroup LspFormatting
-            autocmd! * <buffer>
-            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-        augroup END
-      ]])
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        -- on 0.8, you should use vim.lsp.buf.format instead
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
     end
   end,
 })
