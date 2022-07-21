617fe2d9018141604b5c643c4493b80819223939
Enrique Fueyo <efueyo@lang.ai>
2022-07-21 16:48:29 -0700

    chore(fugitive): set tab spaces in fugitive to identify unsaved gopls formatting

---
+++ nvim/after/ftplugin/fugitive/options.lua
@@ -0,0 +1,8 @@
+local options = {
+  tabstop=4,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -40,7 +40,6 @@ local options = {
 
 for k, v in pairs(options) do
   vim.opt[k] = v
-
 end
 
 vim.cmd [[set wildignore+=*.pyc]]
