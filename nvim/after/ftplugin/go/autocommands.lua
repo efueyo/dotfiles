d3de931b8c54a23b2d5bedd072216c328dbee535
Enrique Fueyo <efueyo@lang.ai>
2022-08-23 17:01:26 -0700

    feat(go): execute wire on wire.go save

--- nvim/after/ftplugin/go/autocommands.lua
+++ nvim/after/ftplugin/go/autocommands.lua
@@ -11,3 +11,31 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = group,
 })
+
+local wire_output_buf = vim.api.nvim_create_buf(false, true)
+-- execute wire on wire.go save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "wire.go",
+  callback = function ()
+    local buf = wire_output_buf
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "wire output:" })
+    local log_data = function (_, data)
+      if data then
+        vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+      end
+    end
+    vim.cmd('botright split')
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win, buf)
+    local cmd = "wire " .. vim.fn.expand("<amatch>:h")
+    vim.fn.jobstart(cmd, {
+      stdout_buffered = true,
+      on_stdout = log_data,
+      on_stderr = log_data,
+      on_exit = function ()
+        vim.api.nvim_buf_set_lines(buf, -1, -1, false, {"wire done", "#######"})
+      end
+    })
+  end,
+  group = group,
+})
