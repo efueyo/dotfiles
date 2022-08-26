d74ec5f4c645f03dc12684a98aa417618fe58bac
Enrique Fueyo <efueyo@lang.ai>
2022-08-26 12:37:20 -0700

    chore(nvim): small cleanup of unused files

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -16,7 +16,7 @@ ls.config.set_config {
   ext_opts = {
     [types.choiceNode] = {
       active = {
-        virt_text = { { " <- Current Choice", "NonTest" } },
+        virt_text = { { " <<-", "NonTest" } },
       },
     },
   },
