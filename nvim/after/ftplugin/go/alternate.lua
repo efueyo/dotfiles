4350d797c00f7a5501afa086544f3b1a18d091b6
Enrique Fueyo <efueyo@lang.ai>
2022-09-27 14:32:44 -0700

    feat(go): set the cursor inside the test function on "go-to-test-suite" mapping

--- nvim/after/ftplugin/go/alternate.lua
+++ nvim/after/ftplugin/go/alternate.lua
@@ -2,11 +2,12 @@ local go_to_alternate = require"user.alternate"
 
 vim.keymap.set("n", "<leader>ga", function () go_to_alternate(".go", "_test.go") end)
 vim.keymap.set("n", "<leader>gav", function () go_to_alternate(".go", "_test.go", "vsplit") end)
--- go to test suite file
+-- go to test suite file and place the cursor in the Test function
 local go_to_test_suite = function ()
   local test_suite_path = vim.fn.expand("%:h") .. "/*suite_test.go"
   vim.cmd("e ".. test_suite_path)
-
+  vim.cmd("/func Test")
+  vim.cmd("noh")
 end
 vim.keymap.set("n", "<leader>ts", function () go_to_test_suite() end)
 
