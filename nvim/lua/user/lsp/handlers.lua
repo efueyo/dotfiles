bfd957a6a82174c0a0b5ee9de5f3eec0d7278efd
Enrique Fueyo <efueyo@lang.ai>
2022-10-28 13:14:16 -0700

    fix(cmp-nvim-lsp): fix capabiltiies warning

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -89,9 +89,8 @@ M.on_attach = function(client, bufnr)
   lsp_highlight_document(client)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
