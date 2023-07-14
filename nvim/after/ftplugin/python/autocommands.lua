33a1f9d471d64f1139d7abc0f360d44411371421
Enrique Fueyo <enrique.fueyo@gmail.com>
2023-07-14 16:53:43 -0700

    feat(python): change noexpandtab to expandtab

--- nvim/after/ftplugin/python/autocommands.lua
+++ nvim/after/ftplugin/python/autocommands.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*.py",
   -- this should be FileType and pattern="python" but it doesn't work and
   -- someone must be overriding shiftwidth
-  command = "setlocal noexpandtab shiftwidth=4 tabstop=4",
+  command = "setlocal expandtab shiftwidth=4 tabstop=4",
   group = group,
 })
