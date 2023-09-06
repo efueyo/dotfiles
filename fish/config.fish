af80646d486cbf70ff828ba4c51248cb4668c859
Enrique Fueyo <enrique.fueyo@gmail.com>
2023-09-06 16:05:19 -0700

    chore(fish): update docker-compose to "docker compose"

--- fish/config.fish
+++ fish/config.fish
@@ -31,8 +31,8 @@ abbr -a g git
 abbr -a gg ginkgo --race --cover --randomize-all -r .
 abbr -a ggu ginkgo --race --cover --randomize-all -r . -- --update
 abbr -a n nvim
-abbr -a dc docker-compose
-abbr -a dcl docker-compose logs --tail=10 -f
+abbr -a dc docker compose
+abbr -a dcl docker compose logs --tail=10 -f
 
 
 if type -q exa
