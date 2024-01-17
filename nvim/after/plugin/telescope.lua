b93466a4aca35a3d0c6f9b827570c443a6fe12a4
Enrique Fueyo <enrique.fueyo@gmail.com>
2024-01-17 14:16:09 -0800

    feat(telescope): <leader>fw and <leader>fW to find current word

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,28 +1,34 @@
-local status_ok, _ = pcall(require, 'telescope')
+local status_ok, _ = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
-
 
 local function nmap(keys, func, desc)
-  vim.keymap.set('n', keys, func, { desc = desc })
+	vim.keymap.set("n", keys, func, { desc = desc })
 end
 
-local builtin = require('telescope.builtin')
-local user_telescope = require('user.telescope')
-local themes = require('telescope.themes')
-nmap('<leader>ff', builtin.find_files, '[F]ind [F]ile')
-nmap('<leader>fg', builtin.git_files, '[F]ind [G]it files')
-nmap('<leader>?', builtin.oldfiles, '[?] Find recently opened files')
-nmap('<leader>fs', builtin.live_grep, '[F]ind [S]earch term')
-nmap('<leader>fb', user_telescope.buffers, '[F]ind [B]uffers')
-nmap('<leader>fd', builtin.diagnostics, '[F]ind [D]iagnostics')
-nmap('<leader>fh', builtin.help_tags, '[F]ind [H]elp')
-nmap('<leader>gc', user_telescope.git_branches, '[G]it [B]ranches')
-nmap('<leader>/', function()
-  builtin.current_buffer_fuzzy_find(themes.get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, '[/] Fuzzily search in current buffer]')
-
+local builtin = require("telescope.builtin")
+local user_telescope = require("user.telescope")
+local themes = require("telescope.themes")
+nmap("<leader>ff", builtin.find_files, "[F]ind [F]ile")
+nmap("<leader>fg", builtin.git_files, "[F]ind [G]it files")
+nmap("<leader>?", builtin.oldfiles, "[?] Find recently opened files")
+nmap("<leader>fs", builtin.live_grep, "[F]ind [S]earch term")
+nmap("<leader>fb", user_telescope.buffers, "[F]ind [B]uffers")
+nmap("<leader>fd", builtin.diagnostics, "[F]ind [D]iagnostics")
+nmap("<leader>fh", builtin.help_tags, "[F]ind [H]elp")
+nmap("<leader>fw", function()
+	local word = vim.fn.expand("<cword>")
+	builtin.grep_string({ search = word })
+end, "[F]ind [w]ord")
+nmap("<leader>fW", function()
+	local word = vim.fn.expand("<cWORD>")
+	builtin.grep_string({ search = word })
+end, "[F]ind [W]ord")
+nmap("<leader>gc", user_telescope.git_branches, "[G]it [B]ranches")
+nmap("<leader>/", function()
+	builtin.current_buffer_fuzzy_find(themes.get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
+end, "[/] Fuzzily search in current buffer]")
