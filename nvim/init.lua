36a09cf8c03a2341287fa6c97011166dfcb8932a
Enrique Fueyo <enrique.fueyo@gmail.com>
2024-03-26 19:44:53 +0100

    chore(nvim): formattin init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,30 @@
 local group = vim.api.nvim_create_augroup("file_types", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.es6",
-  command = "setf javascript",
-  group = group
+	pattern = "*.es6",
+	command = "setf javascript",
+	group = group,
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.tsx",
-  command = "setf typescriptreact",
-  group = group
+	pattern = "*.tsx",
+	command = "setf typescriptreact",
+	group = group,
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = {"*.md", "*.mdx"},
-  command = "setf markdown",
-  group = group
+	pattern = { "*.md", "*.mdx" },
+	command = "setf markdown",
+	group = group,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "yaml",
-  command = "setlocal shiftwidth=2 tabstop=2",
-  group = group
+	pattern = "yaml",
+	command = "setlocal shiftwidth=2 tabstop=2",
+	group = group,
 })
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  command = "%s/\\s\\+$//e",
-  desc = "Clean whitespaces at the end of the lines",
-  group = group
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
+	desc = "Clean whitespaces at the end of the lines",
+	group = group,
 })
 require("user.options")
 -- make sure to load keymaps before the plugins (lazy) so leader is set before lazy
