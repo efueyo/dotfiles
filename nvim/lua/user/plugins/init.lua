dc0d36ba4881e86a5b13bbb369cfabc543aa7e19
Enrique Fueyo <enrique.fueyo@gmail.com>
2024-04-14 18:40:23 -0700

    feat(treesitter): move to its own plugin file

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -16,23 +16,6 @@ return {
 		},
 	},
 
-	{
-		-- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-	},
-
-	{
-		"nvim-treesitter/playground",
-		config = function()
-			require("nvim-treesitter.configs").setup({})
-		end,
-	},
 	-- telescope requirements
 	"nvim-lua/popup.nvim",
 	"nvim-lua/plenary.nvim",
---
+++ nvim/lua/user/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		-- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+	},
+
+	{
+		"nvim-treesitter/playground",
+		config = function()
+			require("nvim-treesitter.configs").setup({})
+		end,
+	},
+}
