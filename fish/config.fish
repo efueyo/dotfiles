17eeb62826c24051ae4a6d32adba1c2794030b73
Enrique Fueyo <enrique.fueyo@gmail.com>
2024-05-10 12:22:28 -0700

    feat(fish): add act as abbrevation to venv activate

--- fish/config.fish
+++ fish/config.fish
@@ -47,6 +47,8 @@ abbr -a aix aichat -r expert:
 
 abbr -a ficp fish_clipboard_copy
 
+abbr -a act source .venv/bin/activate.fish
+
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
