f739513da50fab8810579437192087f75c197a73
Enrique Fueyo <enrique.fueyo@gmail.com>
2024-05-24 11:14:44 -0700

    feat(personal): add zz to center the screen after completing an item

--- nvim/lua/user/personal.lua
+++ nvim/lua/user/personal.lua
@@ -6,23 +6,23 @@ local keymap = vim.keymap.set
 keymap("n", "<leader>et", ":e ~/notes/TODO.md<CR>")
 local todo_group = vim.api.nvim_create_augroup("TODO-au", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "TODO.md",
-	callback = function()
-		keymap("n", "<leader>ci", "jm`kddGp'`", { desc = "[C]omplete [I]tem", buffer = 0 })
-		-- <C-u> is used to clear the range when : is pressed in visual mode
-		keymap("v", "<leader>ci", ":<C-u>'<,'>d<CR>m`Gp'`", { desc = "[C]omplete [I]tem", buffer = 0 })
-		keymap(
-			"n",
-			"<leader>ab",
-			"I- Review Notion<CR>- Review PRs<CR>- Review Linear<CR><ESC>",
-			{ desc = "[A]dd [B]asic items", buffer = 0 }
-		)
-	end,
-	group = todo_group,
+  pattern = "TODO.md",
+  callback = function()
+    keymap("n", "<leader>ci", "jm`kddGp'`zz", { desc = "[C]omplete [I]tem", buffer = 0 })
+    -- <C-u> is used to clear the range when : is pressed in visual mode
+    keymap("v", "<leader>ci", ":<C-u>'<,'>d<CR>m`Gp'`zz", { desc = "[C]omplete [I]tem", buffer = 0 })
+    keymap(
+      "n",
+      "<leader>ab",
+      "I- Review Notion<CR>- Review PRs<CR>- Review Linear<CR><ESC>",
+      { desc = "[A]dd [B]asic items", buffer = 0 }
+    )
+  end,
+  group = todo_group,
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "TODO.md",
-	command = "let b:copilot_enabled = v:false",
-	group = todo_group,
+  pattern = "TODO.md",
+  command = "let b:copilot_enabled = v:false",
+  group = todo_group,
 })
