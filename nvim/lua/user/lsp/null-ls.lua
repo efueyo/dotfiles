61044f8e3b562c77f39a6ff72bb323706ed96af7
Enrique Fueyo <enrique.fueyo@gmail.com>
2024-09-25 12:50:12 -0700

    feat(null-ls): remove extra args from null-ls to use local config

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -10,7 +10,6 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
 			filetypes = {
 				"javascript",
 				"javascriptreact",
