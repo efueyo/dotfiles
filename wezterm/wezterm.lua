de6a6e15a92fa07110bfb1e2c1d71284aa48fd78
efueyo <efueyo@lang.ai>
2024-10-01 16:46:42 -0700

    feat(wezterm): use FiraCode from nerdfonts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("Fira Code"),
+	font = wezterm.font("FiraCode Nerd Font"),
 	font_size = 13.0,
 	color_scheme = "Catppuccin Frappe",
 	hide_tab_bar_if_only_one_tab = true,
