d677cf9b72edfc28f50dc53778c4603923ba4d48
efueyo <efueyo@lang.ai>
2024-10-01 17:02:57 -0700

    feat(nixos): use unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
