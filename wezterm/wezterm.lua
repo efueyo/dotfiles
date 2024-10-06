cfea7ca625d17754ad6ef0374dd0ff3ce52a9136
efueyo <efueyo@lang.ai>
2024-10-06 13:06:04 -0700

    fix(wezterm): add frontend WebGpu to fix blocks.
    More info at https://github.com/wez/wezterm/issues/5990

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,7 @@ return {
 	font_size = 13.0,
 	color_scheme = "Catppuccin Frappe",
 	hide_tab_bar_if_only_one_tab = true,
+  front_end = "WebGpu",
 	window_padding = {
 		left = "0.5cell",
 		right = "0.5cell",
