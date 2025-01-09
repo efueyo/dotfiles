2040dea3b41af8af948786bdd3b89437d7960897
Enrique Fueyo <enrique.fueyo@gmail.com>
2025-01-09 04:08:31 -0800

    chore(lua): fix description for leader-leader-x keymap

--- nvim/after/ftplugin/lua/mappings.lua
+++ nvim/after/ftplugin/lua/mappings.lua
@@ -3,4 +3,4 @@ local keymap = vim.keymap.set
 -- from https://github.com/tjdevries/config.nvim/blob/37c9356fd40a8d3589638c8d16a6a6b1274c40ca/plugin/keymaps.lua
 keymap("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
 keymap("v", "<leader>x", ":lua<CR>", { desc = "Execute the selection line" })
-keymap("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current line" })
+keymap("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
