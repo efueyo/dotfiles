85e9f9006218226f9b70e20e88fc8d68771c7425
Enrique Fueyo <enrique.fueyo@gmail.com>
2025-01-13 18:18:22 -0800

    feat(nvim): leader-x to execute in sh files, not leader-xp

--- nvim/after/ftplugin/sh/mappings.lua
+++ nvim/after/ftplugin/sh/mappings.lua
@@ -1,10 +1,10 @@
 local keymap = vim.keymap.set
 -- execute and print the output of the current line
 -- yank the current line, add two lines below, paste the yanked line in between the lines, execute the line in bash
-keymap("n", "<leader>xp", "yy2o<ESC>kpV:!/bin/bash<CR>", { desc = "e[X]ecute and [P]rint", buffer = 0 })
+keymap("n", "<leader>x", "yy2o<ESC>kpV:!/bin/bash<CR>", { desc = "Execute the current line", buffer = 0 })
 keymap(
 	"v",
-	"<leader>xp",
+	"<leader>x",
 	"y'<P'<O<ESC>'>o<ESC>:<C-u>'<,'>!/bin/bash<CR>",
-	{ desc = "e[X]ecute and [P]rint", buffer = 0 }
+	{ desc = "Execute the current selection", buffer = 0 }
 )
