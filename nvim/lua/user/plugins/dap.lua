cd6393e2e4a5a0c6f0af03cd523496a79da85759
Enrique Fueyo <enrique.fueyo@gmail.com>
2025-02-21 23:53:13 -0800

    feat(nvim): use dap again

---
+++ nvim/lua/user/plugins/dap.lua
@@ -0,0 +1,34 @@
+local keymap = vim.keymap.set
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go",
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local dapgo = require("dap-go")
+		dapui.setup()
+		dapgo.setup()
+		require("nvim-dap-virtual-text").setup()
+
+		keymap("n", "<F1>", dap.step_back)
+		keymap("n", "<F2>", dap.step_into)
+		keymap("n", "<F3>", dap.step_over)
+		keymap("n", "<F4>", dap.step_out)
+		keymap("n", "<F5>", dap.continue)
+		keymap("n", "<leader>b", dap.toggle_breakpoint)
+		keymap("n", "<leader>dt", dapgo.debug_test)
+		keymap("n", "<space>?", function()
+			dapui.eval(nil, { enter = true })
+		end)
+
+		dap.listeners.before.attach.dapui_config = dapui.open
+		dap.listeners.before.launch.dapui_config = dapui.open
+		dap.listeners.before.event_terminated.dapui_config = dapui.close
+		dap.listeners.before.event_exited.dapui_config = dapui.close
+	end,
+}
