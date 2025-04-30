8e91b890d39441f3b0e7a6edcd66db6c7425bb43
Enrique Fueyo <enrique.fueyo@gmail.com>
2025-04-29 20:04:46 -0700

    feat(personal): TODO list as a floating window

--- nvim/lua/user/personal.lua
+++ nvim/lua/user/personal.lua
@@ -3,14 +3,45 @@
 local keymap = vim.keymap.set
 
 -- my TODO file
-keymap("n", "<leader>et", ":e ~/notes/TODO.md<CR>")
 local todo_group = vim.api.nvim_create_augroup("TODO-au", { clear = true })
+local function open_todo_win()
+  local todo = vim.fn.expand("~/notes/TODO.md")
+  local bufn = vim.fn.bufnr(todo, true)
+  local width = math.floor(vim.o.columns * 0.9)
+  local height = math.floor(vim.o.lines * 0.9)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local win_id = vim.api.nvim_open_win(bufn, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+    title = "My TODO list",
+    title_pos = "center",
+  })
+  vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+      if vim.api.nvim_win_is_valid(win_id) then
+        vim.api.nvim_win_close(win_id, true)
+      end
+      return true
+    end,
+    buffer = bufn,
+    group = todo_group,
+  })
+end
+
+keymap("n", "<leader>et", open_todo_win)
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "TODO.md",
   callback = function()
     keymap("n", "<leader>ci", "jm`kddGp'`zz", { desc = "[C]omplete [I]tem", buffer = 0 })
     -- <C-u> is used to clear the range when : is pressed in visual mode
     keymap("v", "<leader>ci", ":<C-u>'<,'>d<CR>m`Gp'`zz", { desc = "[C]omplete [I]tem", buffer = 0 })
+    keymap("n", "Q", "ZZ", { desc = "Save and [Q]uit", buffer = 0 })
     keymap(
       "n",
       "<leader>ab",
