eb1efbe5593aee2f8d91d0a811d3cf04c89cc993
Enrique Fueyo <enrique.fueyo@gmail.com>
2025-09-17 08:55:38 -0700

    feat(wezterm): shift+enter integration for claude

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,18 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("Fira Code"),
-	font_size = 13.0,
-	color_scheme = "Catppuccin Frappe",
-	hide_tab_bar_if_only_one_tab = true,
-	window_padding = {
-		left = "0.5cell",
-		right = "0.5cell",
-		top = "0.25cell",
-		bottom = "0.25cell",
-	},
+  font = wezterm.font("Fira Code"),
+  font_size = 13.0,
+  color_scheme = "Catppuccin Frappe",
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    left = "0.5cell",
+    right = "0.5cell",
+    top = "0.25cell",
+    bottom = "0.25cell",
+  },
+  keys = {
+    { key = "Enter", mods = "SHIFT", action = wezterm.action { SendString = "\x1b\r" } },
+  }
+
 }
