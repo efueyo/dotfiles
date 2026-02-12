9c3eb66c43ca362918cb54b5a15fde1ffcef877c
Enrique Fueyo <enrique.fueyo@gmail.com>
2026-02-11 21:15:24 -0800

    fix(nvim): move mason setup_handlers to handlers property in setup

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -135,16 +135,15 @@ local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
   ensure_installed = vim.tbl_keys(servers),
-})
-
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    })
-  end,
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+      })
+    end,
+  },
 })
 
 require("user.lsp.null-ls")
