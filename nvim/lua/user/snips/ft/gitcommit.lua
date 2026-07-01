76bf868dc4566c313e0cc8973542c19b72b5e382
Enrique Fueyo <enrique.fueyo@gmail.com>
2026-07-01 08:59:49 -0700

    feat(nvim): update gitcommit snip for branchtag

--- nvim/lua/user/snips/ft/gitcommit.lua
+++ nvim/lua/user/snips/ft/gitcommit.lua
@@ -6,36 +6,37 @@ local f = ls.f
 local i = ls.insert_node
 
 local capture_cmd = function(cmd)
-	local file = io.popen(cmd, "r")
-	local content = assert(file:read("*a"))
-	file:close()
-	content = string.gsub(content, "^%s+", "")
-	content = string.gsub(content, "[\n\r]+", " ")
-	content = string.gsub(content, "%s+$", "")
-	return content
+  local file = io.popen(cmd, "r")
+  local content = assert(file:read("*a"))
+  file:close()
+  content = string.gsub(content, "^%s+", "")
+  content = string.gsub(content, "[\n\r]+", " ")
+  content = string.gsub(content, "%s+$", "")
+  return content
 end
 
 return {
-	s("feat", { t("feat("), i(1), t("): "), i(2) }),
-	s("fix", { t("fix("), i(1), t("): "), i(2) }),
-	s("refactor", { t("refactor("), i(1), t("): "), i(2) }),
-	s("chore", { t("chore("), i(1), t("): "), i(2) }),
-	s("hack", { t("hack("), i(1), t("): "), i(2) }),
-	s("test", { t("test("), i(1), t("): "), i(2) }),
-	s("docs", { t("docs("), i(1), t("): "), i(2) }),
-	s("branchtag", {
-		f(function()
-			local branch = capture_cmd("git branch --show-current")
-			-- from branch names like feature/abc-123-include-this-feature
-			-- extract #ABC-123
-			local tag = string.gsub(branch, "^.*/(%a+)-(%d+).*", function(c1, c2)
-				return string.upper(c1) .. "-" .. c2
-			end)
+  s("feat", { t("feat("), i(1), t("): "), i(2) }),
+  s("fix", { t("fix("), i(1), t("): "), i(2) }),
+  s("refactor", { t("refactor("), i(1), t("): "), i(2) }),
+  s("chore", { t("chore("), i(1), t("): "), i(2) }),
+  s("hack", { t("hack("), i(1), t("): "), i(2) }),
+  s("test", { t("test("), i(1), t("): "), i(2) }),
+  s("docs", { t("docs("), i(1), t("): "), i(2) }),
+  s("branchtag", {
+    f(function()
+      local branch = capture_cmd("git branch --show-current")
+      -- from branch names like feature/abc-123-include-this-feature
+      -- or abc-123-include-this-feature, extract #ABC-123
+      local proj, num = branch:match("(%a+)%-(%d+)")
+      if proj and num then
+        return "#" .. string.upper(proj) .. "-" .. num
+      end
 
-			return "#" .. tag
-		end),
-	}),
-	s("noci", { t("[no ci]") }),
-	s("wip", { t("#WIP") }),
-	s("revertme", { t("#REVERTME") }),
+      return "#"
+    end),
+  }),
+  s("noci", { t("[no ci]") }),
+  s("wip", { t("#WIP") }),
+  s("revertme", { t("#REVERTME") }),
 }
